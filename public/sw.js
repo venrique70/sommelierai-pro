b079e286d2ea1aff191bfc44419e970a4a94122b
venrique70 <venrique70@gmail.com>
2026-02-04 09:11:18 -0500

    chore(pwa): add fetch handler to minimal service worker
    chore(pwa): add fetch handler to minimal service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
 // SW minimalista para habilitar modo instalable (sin cache aún)
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", () => {});
